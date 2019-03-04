a554bd5f87bb98e49814dc5226fd95c30aebe643
titouandubuc <panodu92>
2019-03-04 16:00:43 +0100

    test Starwars fin

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -9,7 +9,6 @@ describe('starwars-names', () => {
     test('should be an array of strings', () => {
       for(var i=0;i<starWarsNames.length;i++){
         expect(typeof "starWarsNames[i]").toBe("string");
-
       }
       
     });
@@ -20,17 +19,17 @@ describe('starwars-names', () => {
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).not.toContain("Ben Quadinaros");
     });
   });
 
-  describe('random', () => {
+    describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toContain(starWars.random());
     });
 
     test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toEqual(expect.arrayContaining(starWars.random(Math.floor((Math.random()*10)))))
     });
   });
 });
